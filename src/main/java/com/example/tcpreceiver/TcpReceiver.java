deb487f4c0d6f8af83ac67eb05e5b661a6786643
asyifa <asyifanbd@gmail.com>
2024-03-12 10:52:54 +0700

    add thread

---
+++ src/main/java/com/example/tcpreceiver/TcpReceiver.java
@@ -0,0 +1,29 @@
+package com.example.tcpreceiver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TcpReceiver implements  Runnable {
+
+    private int port;
+
+    public TcpReceiver(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String data = reader.readLine();
+                System.out.println("Received data: " + data);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/example/tcpreceiver/TcpReceiverApplication.java
+++ src/main/java/com/example/tcpreceiver/TcpReceiverApplication.java
@@ -1,9 +1,5 @@
 package com.example.tcpreceiver;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,15 +9,6 @@ public class TcpReceiverApplication {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(TcpReceiverApplication.class, args);
-
-        try (ServerSocket serverSocket = new ServerSocket(8047)) {
-            while (true) {
-                Socket socket = serverSocket.accept();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String data = reader.readLine();
-                System.out.println("Received data: " + data);
-            }
-        }
+        new Thread(new TcpReceiver(5678)).start();
     }
-
 }
